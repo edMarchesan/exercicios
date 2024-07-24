bfb6fd676cab556fd1471c1a61d08c6e8a353330
edMarchesan <marchesan@pm.me>
2024-07-24 20:13:14 -0300

    correcao da lista 09 em aula para error quando o usuario inseria uma base 0 que retornava erro e quando o usuario inseria um expoente 0 que dava duas mensagens iguais,

--- l06e09.c
+++ l06e09.c
@@ -5,16 +5,10 @@ int main(void) {
   printf("Digite a base e em seguida o expoente.\n");
   scanf("%d", &b);
   scanf("%d", &e);
-  vlr = b;
-  if (e == 0) {
-    printf("A potencia de %d sobre %d e 1 \n", b, e);
-  } else {
-
-    for (i = 1; i <= e; i++) {
-      vlr *= b;
-    }
+  vlr = 1;
+  for (i = 1; i <= e; i++) {
+    vlr *= b;
   }
-  vlr /= b;
   printf("A potencia de %d sobre %d e %d \n", b, e, vlr);
 
   return 0;
